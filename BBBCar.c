e9e084b5baf6971dfe9dddaf0a31323ccd85afcc
Siromitu26 <siromitu26@gmail.com>
2014-03-13 06:36:29 +0000

    added Makefile, BBBCar.c, Directory "etc".

---
+++ BBBCar.c
@@ -0,0 +1,58 @@
+﻿#include "BBBCar.h"
+#include <BBB.h>
+#include <BBB_GPIO.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void BBB_gpioArray_init(BBB_gpio **paramGpio, char **paramPinName, int paramPinNum){
+	int i = 0;
+	while(i < paramPinNum){
+		if((paramGpio[i] = BBB_open_gpio(paramPinName[i])) == NULL ) {
+			printf("Can not open %s\n", paramPinName[i] );
+			exit(EXIT_FAILURE);
+		}
+		paramGpio[i]->set_direction(paramGpio[i], OUT);
+		paramGpio[i]->put(paramGpio[i], 0);
+		i++;
+	}
+}
+
+void BBB_gpioArray_close(BBB_gpio **paramGpio, int paramPinNum){
+	int i = 0;
+	while(i < paramPinNum){
+		BBB_close_gpio(paramGpio[i++]);
+	}
+}
+
+void motorControl(BBB_gpio **paramGpio, int paramData){
+/* *pinはモーター制御用の信号ピンを制御するBBB_gpioの配列（要素は2つ） */
+/* dataのビット値でモーターの制御をする	*/
+/* 		00 : フリー		*/
+/* 		01 : 正回転		*/
+/* 		10 : 逆回転		*/
+/* 		11 : ブレーキ		*/
+	switch(paramData){
+		case 0:	//00
+		paramGpio[0]->put(paramGpio[0], 0);
+		paramGpio[1]->put(paramGpio[1], 0);
+		break;
+
+		case 1:	//01
+		paramGpio[0]->put(paramGpio[0], 0);
+		paramGpio[1]->put(paramGpio[1], 0);
+		break;
+	
+		case 2:	//10
+		paramGpio[0]->put(paramGpio[0], 0);
+		paramGpio[1]->put(paramGpio[1], 0);
+		break;
+
+		case 3:	//11
+		paramGpio[0]->put(paramGpio[0], 0);
+		paramGpio[1]->put(paramGpio[1], 0);
+		break;
+
+		default:
+		break;
+	}
+}
---
+++ Makefile
@@ -0,0 +1,16 @@
+﻿CC=gcc
+OBJS=main.o BBBCar.o
+CFLAGS=-W -Wall -lBBB
+PROGRAM=BBBCarControlServer 
+RM=rm -f
+.SUFFIXES: .c .o
+
+$(PROGRAM): $(OBJS)
+	$(CC) -o $@ $^ $(CFLAGS)
+.c .o:
+	$(CC) -c $< $(CFLAGS)
+
+.PHONY: clean
+clean:
+	$(RM) $(PROGRAM) $(OBJS)
+	
